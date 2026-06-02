97e5c7989da4ed20e14dc949ef32db181a336481
Wwen111 <3863712474@qq.com>
2026-06-02 23:46:33 +0800

    second commit

--- src/com/Test704.java
+++ src/com/Test704.java
@@ -22,4 +22,10 @@ public class Test704 {
         }
         return -1;
     }
+    public static void main(String[] args) {
+        Test704 t = new Test704();
+        int[] arr={1,3,5,7,9};
+        int res = t.search(arr,5);
+        System.out.println("查到下标："+res);
+    }
 }
