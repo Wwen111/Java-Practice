a8a96a5d03ffb9359338a4df3c6ac4518709e9e1
Wwen111 <3863712474@qq.com>
2026-06-23 22:38:17 +0800

    update file name

--- src/com/Array/removeElement.java
+++ src/com/Array/removeElement.java
@@ -1,6 +1,6 @@
 package com.Array;
 
-public class BinarySearch {
+public class removeElement {
     public int removeElement(int[] nums, int val) {
         int slow=0;
         for(int fast=0;fast<nums.length;fast++)
@@ -14,7 +14,7 @@ public class BinarySearch {
         return slow;
     }
     public static void main(String[] args) {
-        BinarySearch obj = new BinarySearch();
+        removeElement obj = new removeElement();
         int[] arr = {3,2,2,3};
         int res = obj.removeElement(arr,3);
         System.out.println("新数组长度："+res);
