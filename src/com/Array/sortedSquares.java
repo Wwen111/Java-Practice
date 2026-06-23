720b5da5e10ab7752114b92a498a0f1be447d6bc
Wwen111 <3863712474@qq.com>
2026-06-23 22:45:01 +0800

    update file name

---
+++ src/com/Array/sortedSquares.java
@@ -0,0 +1,32 @@
+package com.Array;
+
+public class sortedSquares {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int pos = result.length - 1;
+        int right = nums.length - 1;
+        int left = 0;
+        while (left <= right) {
+            if (nums[left] * nums[left] > nums[right] * nums[right]) {
+                result[pos] = nums[left] * nums[left];
+                pos--;
+                left++;
+            } else {
+                result[pos] = nums[right] * nums[right];
+                pos--;
+                right--;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        sortedSquares obj = new sortedSquares();
+        int[] arr = {-4, -1, 0, 3, 10};
+        int[] ans = obj.sortedSquares(arr);
+        System.out.print("平方排序结果：");
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+}
